82b2e45c7a217eaba40c33b60c8d03b200de126d
Brent Johnson <brent_johnson@abercrombie.com>
2016-02-20 16:08:10 -0500

    More fields.  Playing with layout

--- animals/model.js
+++ animals/model.js
@@ -8,17 +8,57 @@ Meteor.animals.attachSchema(new SimpleSchema({
 	name: {
 		type: String,
 		label: "Name",
+		min: 1,
 		max: 64
+	},
+	tagNumber: {
+		type: String,
+		label: "Tag Number",
+		optional: true
+	},
+	licenseNumber: {
+		type: String,
+		label: "License Number",
+		optional: true
+	},
+	microchipNumber: {
+		type: String,
+		label: "Microchip Number",
+		optional: true
+	},
+	breed: {
+		type: String,
+		label: "Breed"
 	},
 	gender: {
 		type: String,
 		label: "Gender",
 		allowedValues: ["male", "female"]
 	},
+	intakeDate: {
+		type: Date,
+		label: "Intake Date"
+	},
+	dateOfBirth: {
+		type: Date,
+		label: "Date of Birth",
+		optional: true
+	},
 	location: {
 		type: String,
 		label: "Location",
 		allowedValues: ["boarding", "foster", "shelter", "adopted"]
+	},
+	coordinator: {
+		type: String,
+		label: "Coordinator",
+		allowedValues: ["Carol", "Carrie", "Kara", "Leslie", "Niki"],
+		optional: true
+	},
+	foster: {
+		type: String,
+		label: "Foster",
+		optional: true
 	}
 
 }));
