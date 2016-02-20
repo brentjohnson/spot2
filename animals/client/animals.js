61a5e87ffd64bd11d3b882b44feb21315d691a1e
Brent Johnson <brent_johnson@abercrombie.com>
2016-02-20 11:07:43 -0500

    Some dynamic data in the sidebar

--- animals/client/animals.js
+++ animals/client/animals.js
@@ -1,4 +1,4 @@
-Template.animals.onCreated(function () {
+Template.mainLayout.onCreated(function () {
   this.subscribe("animals")
 })
 
@@ -16,4 +16,28 @@ Template.animalDetail.helpers({
   animalDetail: function () {
     return Meteor.animals.find();
   }
+})
+
+
+Template.sidebarMenu.helpers({
+	allAnimals: function() {
+		return Meteor.animals.find();
+	},
+
+	males: function() {
+		return Meteor.animals.find({gender: "male"});
+	},
+
+	females: function() {
+		return Meteor.animals.find({gender: "female"});
+	},
+	
+	inBoardings: function() {
+		return Meteor.animals.find({inBoarding: "true"});
+	},
+	
+	inFosters: function() {
+		return Meteor.animals.find({inFoster: "true"});
+	}
+	
 })
